93fdb0acd31043a2bbdec5e9850725564814ffca
carsonyarborough <carson.yarborough@gmail.com>
2023-07-13 22:00:33 -0500

    start os js

---
+++ script.js
@@ -0,0 +1,13 @@
+let nav = 0;
+let clicked = null;
+let events = localStorage.getitem('events') ? JSON.parse(local.localStorage.getitem('events')) : [];
+
+const calendar = document.getElementById('calendar');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function load() {
+
+
+}
+
+load();
