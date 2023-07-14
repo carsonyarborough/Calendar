ffde249e0e45848eb8f18c92d444f8f8b984e34d
carsonyarborough <carson.yarborough@gmail.com>
2023-07-13 22:07:24 -0500

    dates function

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ const calendar = document.getElementById('calendar');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 function load() {
+const dt = new Date();
 
+const day = dt.getDate();
+const month = dt.getMonth();
+const year = dt.getFullYear();
+
+const firstDayOfMonth = new Date(year, month, 1);
+const daysInMonth = new Date(year, month + 1, 0).getDate();
 
 }
 
