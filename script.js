f337547a671401197429ac6fa4e6e50cdb857110
carsonyarborough <carson.yarborough@gmail.com>
2023-07-14 15:55:45 -0500

    days and padding days js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let nav = 0;
 let clicked = null;
-let events = localStorage.getitem('events') ? JSON.parse(local.localStorage.getitem('events')) : [];
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -15,6 +15,29 @@ const year = dt.getFullYear();
 const firstDayOfMonth = new Date(year, month, 1);
 const daysInMonth = new Date(year, month + 1, 0).getDate();
 
+const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+weekday: 'long',
+year: 'numeric',
+month: 'numeric',
+day: 'numeric',
+
+});
+ const paddingDays = weekdays.indexOf(dateString.split(',')[0]);
+
+ for(let i = 1; i <= paddingDays + daysInMonth; i++){
+    const daySquare = document.createElement('div');
+    daySquare.classList.add('day');
+
+if(i > paddingDays){
+    daySquare.innerText = i - paddingDays;
+
+    daySquare.addEventListener('click', () => console.log('click'));
+}   else{
+    daySquare.classList.add('padding');
+}
+    calendar.appendChild(daySquare);
+
+ }
 }
 
 load();
